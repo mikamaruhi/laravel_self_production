e5de7f1bc41c4541382e48980738aa8514923369
mikamaruhi <145239524+mikamaruhi@users.noreply.github.com>
2024-02-22 23:09:05 +0900

    modelsの修正変更2

--- app/Models/property.php
+++ app/Models/property.php
@@ -1,40 +1,24 @@
 <?php
+
+namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
     use HasFactory;
-
     protected $table = 'properties';
 
     protected $fillable = [
-        'property_id',
-        'property_name',
-        'responsible_id',
-        'responsible_name',
-        'accounting_person_name',
+        'property_id' ,
+        'property_name' ,
+        'responsible_id' ,
+        'responsible_name' ,
+        'accounting_person_name' ,
+
     ];
 }
-
-// namespace App\Models;
-
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
-
-// class Property extends Model
-// {
-//     use HasFactory;
-//     protected $table = 'properties';
-
-//     protected $fillable = [
-//         'property_id' ,
-//         'property_name' ,
-//         'responsible_id' ,
-//         'responsible_name' ,
-//         'accounting_person_name' ,
-
-//     ];
     
     // // 物件は担当者がついている（所属）
     // public function user(){
