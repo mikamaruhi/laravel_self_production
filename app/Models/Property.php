ab7ec513dfc5aaf6163024601e322371b19b3141
mikamaruhi <145239524+mikamaruhi@users.noreply.github.com>
2024-02-23 11:24:03 +0900

    modelsの修正

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    use HasFactory;
+    use HasFactory; 
     protected $table = 'properties';
 
     protected $fillable = [
